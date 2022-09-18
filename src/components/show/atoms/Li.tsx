dca39cde28735b3cc8a408a4a40e0d428e588901
youdaehoon <107843412+youdaehoon@users.noreply.github.com>
2022-09-18 11:23:08 +0900

    create components(Label,List,MarkdownList)

---
+++ src/components/show/atoms/Li.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+interface IProps {
+  fontSize?: Number;
+  color?: string;
+  children:JSX.Element,
+}
+const Li = ({ fontSize: f, color: c ,children}: IProps) => {
+    console.log(f,"durl")
+  return <MyDot fontSize={f} color={c}>{children}</MyDot>;
+};
+
+export default Li;
+
+const MyDot = styled.li<IProps>`
+  color: ${(props) => props.color?props.color:"black"};
+  font-size:  ${(props) => props.fontSize?props.fontSize.toString()+"px":"black"};
+`;
--- src/pages/ShowPage.tsx
+++ src/pages/ShowPage.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Label from '../components/show/atoms/Label'
+import MarkdownList from '../components/show/molecules/MarkdownList'
+
+
 
 const ShowPage = () => {
   return (
-    <div>ShowPage</div>
+    <>
+    <MarkdownList fontSize={20} text="테스트" width={30} ></MarkdownList>
+    </>
+    
   )
 }
 
